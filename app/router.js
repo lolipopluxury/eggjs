9032a59755550968ffff35b53a797140945f1e24
lolipopluxury <songyupeng@yahoo.com>
2020-06-22 22:17:34 +0800

    router update

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ module.exports = app => {
   const interceptor = app.middleware.interceptor() //isolated middleware 
   router.get('/',jwt,interceptor, controller.home.index);
   router.get('/token',controller.csrf.getToken);
+  router.get('/logout',jwt,interceptor,controller.user.logout);
   router.post('/encryption',controller.home.encryption);
   router.post('/decryption',controller.home.decryption);
   router.post('/register',controller.user.register);
