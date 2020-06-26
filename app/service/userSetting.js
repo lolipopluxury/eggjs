b429dfcb0c4b1becf836dd0350c3c0b9bb10b595
lolipopluxury <songyupeng@yahoo.com>
2020-06-26 21:00:19 +0800

    update

---
+++ app/service/userSetting.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class UserSettingService extends Service {
+  async update(){
+    return 
+  }
+}
+
+module.exports = UserSettingService;
